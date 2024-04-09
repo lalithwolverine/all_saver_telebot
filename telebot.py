6b47f8dcb18c2a23c61a54062b17451c8544a12b
Lalith D <dlalith422@gmail.com>
2024-04-10 00:05:25 +0530

    1st trial failed

--- telebot.py
+++ telebot.py
@@ -1,29 +1,45 @@
 from typing import Final
 from telegram import Update
-from telegram.ext import CommandHandler, Application, MessageHandler,filters,ContextTypes
+from telegram.ext import CommandHandler, Updater, MessageHandler,filters
 import requests
 from pytube import YouTube
 
-# TOKEN :Final = '6982432240:AAGohongzRMIvf8YVVa1Zirfi8HZ2aNEDEk'
-# BOT_USERNAME : Final = '@lalith_saver_bot'
+TOKEN :Final = '6982432240:AAGohongzRMIvf8YVVa1Zirfi8HZ2aNEDEk'
+BOT_USERNAME : Final = '@lalith_saver_bot'
 
-# #Commands
-# async def start_command(update : Update, context : ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("Hello !\n This is Lalith's Bot !!\n Enjoy downloading!!!")
+#Commands
+async def start_command(update : Update, context ):
+    await update.message.reply_text("Hello !\n This is Lalith's Bot !!\n Enjoy downloading!!!")
 
 
-# async def help_command(update : Update, context : ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text("Paste any valid link to download media content :)")
+async def help_command(update : Update, context ):
+    await update.message.reply_text("Paste any valid link to download media content :)")
 
     
-# #Responses
-# def handle_respnse(text : str) ->str:
-#     processed : str= text.lower()
+#Responses
+def handle_respnse(text : str) ->str:
+    processed : str= text.lower()
 
-#     if 'Hello' in processed:
-#         return ''
+    if 'youtu' in processed:
+        yt=YouTube(text)
+        ys=yt.streams.get_highest_resolution()
+        return ys.url
+    
+def download_media(update: Update, context):
+    url = update.message.text
+    response = handle_response(url)
+    update.message.reply_text(response)
 
-link=input("Enter youtbe link: ")
-yt=YouTube(link)
-print(yt.streams.filter(only_audio=True))
-print(yt.streams.filter(only_video=True))
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start_command))
+    dp.add_handler(CommandHandler("help", help_command))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, download_media))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
+
+ 
